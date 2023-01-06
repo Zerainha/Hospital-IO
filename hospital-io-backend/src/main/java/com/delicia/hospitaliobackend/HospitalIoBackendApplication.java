c0f69ebbc909e1356809ce1c68dc6ec6e8b8d62e
Breno <meliantesalvador@gmail.com>
2023-01-06 09:22:53 -0300

    Primeiro commit back-end

---
+++ hospital-io-backend/src/main/java/com/delicia/hospitaliobackend/HospitalIoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.delicia.hospitaliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalIoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalIoBackendApplication.class, args);
+	}
+
+}
---
+++ hospital-io-backend/src/test/java/com/delicia/hospitaliobackend/HospitalIoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delicia.hospitaliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalIoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
